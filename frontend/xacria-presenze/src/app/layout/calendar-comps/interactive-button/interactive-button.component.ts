4b11303b3dd5d669b8a35cd115d7154225895930
Samuele73 <samuelemoscuzza73@gmail.com>
2025-09-13 16:59:33 +0200

    Setup interactive button to filter calendar entries based on the type and button mode

--- frontend/xacria-presenze/src/app/layout/calendar-comps/interactive-button/interactive-button.component.ts
+++ frontend/xacria-presenze/src/app/layout/calendar-comps/interactive-button/interactive-button.component.ts
@@ -4,6 +4,15 @@ import { AvailabilityModalComponent } from '../modals/availability-modal/availab
 import { RequestModalComponent } from '../modals/request-modal/request-modal.component';
 import { WorkingTripModalComponent } from '../modals/working-trip-modal/working-trip-modal.component';
 import { DayworkModalComponent } from '../modals/daywork-modal/daywork-modal.component';
+import {
+  identifiableCalendarAvailability,
+  identifiableCalendarEntry,
+  identifiableCalendarRequest,
+  identifiableCalendarWorkingDay,
+  identifiableCalendarWorkingTrip,
+} from '../../shared/models/calendar';
+import { DateFormatService } from 'src/app/shared/services/date-format.service';
+import { CalendarEntry } from 'src/generated-client';
 
 declare var bootstrap: any;
 const faIcons = { plus: faPlus, minus: faMinus };
@@ -22,17 +31,43 @@ type buttonMode = 'ADD' | 'DELETE';
 })
 export class InteractiveButtonComponent implements AfterViewInit {
   faIcons: any = faIcons;
-  @Input() day!: { date: Date };
+  @Input() set date(value: Date | undefined) {
+    this._date = value ? new Date(value) : undefined;
+    this.recompute();
+  }
+  private _date?: Date;
   @Input() dayClicked: Date = new Date();
   @Input() dayWorkDateTitle!: string;
   @Input() mode: buttonMode = 'ADD';
+  @Input() set modalCalendarEntries(
+    value:
+      | Array<
+          | identifiableCalendarWorkingDay
+          | identifiableCalendarRequest
+          | identifiableCalendarWorkingTrip
+          | identifiableCalendarAvailability
+        >
+      | undefined
+  ) {
+    this._allEntries = value ?? [];
+    this.recompute();
+  }
+  private _allEntries: Array<
+    | identifiableCalendarWorkingDay
+    | identifiableCalendarRequest
+    | identifiableCalendarWorkingTrip
+    | identifiableCalendarAvailability
+  > = [];
   @ViewChild('working_trip_modal') workingTripModal!: WorkingTripModalComponent;
   @ViewChild('availability_modal')
   availabilityModal!: AvailabilityModalComponent;
   @ViewChild('request_modal') requestModal!: RequestModalComponent;
   @ViewChild('daywork_modal') dayworkModal!: DayworkModalComponent;
+  filteredEntries: Array<identifiableCalendarEntry> = [];
 
-  get isModalsModifyMode() : boolean {
+  constructor(private dateFormat: DateFormatService) {}
+
+  get isModalsModifyMode(): boolean {
     return this.mode === 'DELETE';
   }
 
@@ -59,5 +94,40 @@ export class InteractiveButtonComponent implements AfterViewInit {
       console.log('modal date:', modal.currentDate);
     }
     modal.open();
+  }
+
+  private hasDateRange(entry: any): entry is { dateFrom: any; dateTo: any } {
+    return entry && 'dateFrom' in entry && 'dateTo' in entry;
+  }
+
+  private areWorkingDayEntries(entry: any): entry is { dateFrom: any } {
+    return entry && 'dateFrom' in entry;
+  }
+
+  // Recompute the filtered entries based on the current date and all entries
+  private recompute(): void {
+    if (!this._date || !this._allEntries || this.isModalsModifyMode) {
+      this.filteredEntries = [];
+      //console.log('No date or entries provided');
+      return;
+    }
+
+    const current = this.dateFormat.normalizeDate(this._date);
+    this.filteredEntries = this._allEntries.filter(
+      (entry: identifiableCalendarEntry) => {
+        const calendarEntry: CalendarEntry = entry.calendarEntry;
+        if (!this.hasDateRange(calendarEntry)) {
+          if (this.areWorkingDayEntries(calendarEntry)) {
+            const from = this.dateFormat.normalizeDate(calendarEntry.dateFrom);
+            //console.log('Current date:', current, 'From date:', from, "result", current === from);
+            return current.getTime() === from.getTime();
+          }
+          return false;
+        }
+        const from = this.dateFormat.normalizeDate(calendarEntry.dateFrom);
+        const to = this.dateFormat.normalizeDate(calendarEntry.dateTo);
+        return current >= from && current <= to;
+      }
+    );
   }
 }
