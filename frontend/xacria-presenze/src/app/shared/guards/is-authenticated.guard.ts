3b6e78384d9a9ed6d7d2a8dbcde14c5e16b6d0c5
Samuele73 <samuelemoscuzza73@gmail.com>
2025-10-18 10:59:55 +0200

    fix guard bug

--- frontend/xacria-presenze/src/app/shared/guards/is-authenticated.guard.ts
+++ frontend/xacria-presenze/src/app/shared/guards/is-authenticated.guard.ts
@@ -1,28 +1,30 @@
 import { Injectable } from '@angular/core';
 import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot, UrlTree } from '@angular/router';
-import { Observable, catchError, map, tap, throwError } from 'rxjs';
+import { Observable, catchError, map, of, tap, throwError } from 'rxjs';
 import { AuthService } from '../services/auth.service';
 
 @Injectable({
   providedIn: 'root'
 })
 export class IsAuthenticatedGuard implements CanActivate {
+  constructor(private authService: AuthService, private router: Router) {}
 
-  constructor(private authService: AuthService, private router: Router){}
+  canActivate(): Observable<boolean> {
+    const check$ = this.authService.checkUserAutentication2();
 
-  canActivate(
-    route: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
-    return this.authService.checkUserAutentication2()!.pipe(
-      catchError((err: any) => {
-        this.router.navigate(["login"]); //Controllare meglio
-        throw "Errore nell'autenticazione del token dell'utente";
+    if (!check$) {
+      this.router.navigate(['login']);
+      return of(false);
+    }
+
+    return check$.pipe(
+      catchError(() => {
+        this.router.navigate(['login']);
+        return of(false);
       }),
       map((isLoggedIn: any) => {
-        console.log("isloggedin", isLoggedIn);
-        if(!isLoggedIn){
-          console.log("User is not autheticated!")
-          this.router.navigate(["login"]);
+        if (!isLoggedIn) {
+          this.router.navigate(['login']);
           return false;
         }
         this.authService.email = isLoggedIn.email;
@@ -30,5 +32,4 @@ export class IsAuthenticatedGuard implements CanActivate {
       })
     );
   }
-
 }
