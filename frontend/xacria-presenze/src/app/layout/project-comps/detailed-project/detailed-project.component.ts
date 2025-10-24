cb7887fcef79e1f98c5ac9c6ff9f506056422038
Samuele73 <samuelemoscuzza73@gmail.com>
2025-10-24 18:46:38 +0200

    Add translations

--- frontend/xacria-presenze/src/app/layout/project-comps/detailed-project/detailed-project.component.ts
+++ frontend/xacria-presenze/src/app/layout/project-comps/detailed-project/detailed-project.component.ts
@@ -1,27 +1,32 @@
 import { HttpErrorResponse } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { Project } from 'src/generated-client';
 import { ProjectService } from 'src/generated-client/api/api';
 
 @Component({
   selector: 'app-detailed-project',
   templateUrl: './detailed-project.component.html',
-  styleUrls: ['./detailed-project.component.scss']
+  styleUrls: ['./detailed-project.component.scss'],
 })
 export class DetailedProjectComponent implements OnInit {
   project: Project | null = null;
-  backupTitle: string = "Il progetto non è stato trovato";
-  statusCode: string = "";
+  backupTitle: string = 'Il progetto non è stato trovato';
+  statusCode: string = '';
 
-  constructor(private route: ActivatedRoute, private projectService: ProjectService) { }
+  constructor(
+    private route: ActivatedRoute,
+    private projectService: ProjectService,
+    private authService: AuthService
+  ) {}
 
   ngOnInit() {
     this.getProjectFromQueryParams();
   }
 
   private getProjectFromQueryParams() {
-    this.route.queryParams.subscribe(params => {
+    this.route.queryParams.subscribe((params) => {
       const projectId = params['id'];
       if (projectId) {
         this.projectService.getProjectById(projectId).subscribe({
@@ -32,20 +37,17 @@ export class DetailedProjectComponent implements OnInit {
           },
           error: (err: HttpErrorResponse) => {
             console.error('Error loading project:', err);
-            if(err.status === 404){
-              this.backupTitle = "Progetto non trovato";
+            if (err.status === 404) {
+              this.backupTitle = 'Progetto non trovato';
             }
             this.statusCode = err.status.toString();
             this.project = null;
-          }
+          },
         });
-      }
-      else{
+      } else {
         this.project = null;
         console.warn('No project ID found in query parameters.');
       }
-
     });
   }
-
 }
