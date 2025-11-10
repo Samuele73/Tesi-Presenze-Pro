d52cba5fa0dafcb85d6f1502d888a555930a5f7a
Samuele73 <samuelemoscuzza73@gmail.com>
2025-11-10 23:54:31 +0100

    Add translation; make it so that lang does not change when refreshing page

--- frontend/xacria-presenze/src/app/app.component.ts
+++ frontend/xacria-presenze/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { TranslateService } from '@ngx-translate/core';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'xacria-presenze';
+
+  constructor(private translate: TranslateService) {
+    const savedLang = localStorage.getItem('lang') || 'it';
+    translate.addLangs(['it', 'en']);
+    translate.setDefaultLang('it');
+    translate.use(savedLang);
+  }
 }
