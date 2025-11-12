dba37fc2f815016aa1598954cfe0043ee79a7474
Samuele73 <samuelemoscuzza73@gmail.com>
2025-11-12 18:44:17 +0100

    hide request id on modal for not owners

--- frontend/xacria-presenze/src/app/modules/requests-approval/components/request-details-modal/request-details-modal.component.ts
+++ frontend/xacria-presenze/src/app/modules/requests-approval/components/request-details-modal/request-details-modal.component.ts
@@ -1,6 +1,7 @@
 import { Component, Input } from '@angular/core';
 import { FormGroup } from '@angular/forms';
 import { NgbActiveModal } from '@ng-bootstrap/ng-bootstrap';
+import { AuthService } from 'src/app/shared/services/auth.service';
 import { UserRequestResponseDto } from 'src/generated-client';
 
 type handleRequestMode = 'ACCEPT' | 'REFUSE'
@@ -14,7 +15,7 @@ export class RequestDetailsModalComponent {
   @Input() request?: UserRequestResponseDto;
   requestForm!: FormGroup;
 
-  constructor(public activeModal: NgbActiveModal) {}
+  constructor(public activeModal: NgbActiveModal, public authService: AuthService) {}
 
   trackByIndex(index: number): number {
     return index;
