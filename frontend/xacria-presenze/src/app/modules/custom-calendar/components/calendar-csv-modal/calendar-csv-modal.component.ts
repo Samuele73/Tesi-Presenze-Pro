38cf0efd42e9153da9c5c5707ec505e43a6633d8
Samuele73 <samuelemoscuzza73@gmail.com>
2025-11-20 12:48:38 +0100

    create input for csv expot

--- frontend/xacria-presenze/src/app/modules/custom-calendar/components/calendar-csv-modal/calendar-csv-modal.component.ts
+++ frontend/xacria-presenze/src/app/modules/custom-calendar/components/calendar-csv-modal/calendar-csv-modal.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, Input, TemplateRef, ViewChild } from '@angular/core';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { Modal } from 'bootstrap';
 
 @Component({
   selector: 'app-calendar-csv-modal',
@@ -6,10 +8,33 @@ import { Component } from '@angular/core';
   styleUrls: ['./calendar-csv-modal.component.scss']
 })
 export class CalendarCsvModalComponent {
+  @ViewChild('addProjectModal', { static: true }) modalElement!: TemplateRef<Modal>;
+  @Input() currentDate?: Date;
 
-  constructor() { }
+  constructor(private modalService: NgbModal) { }
 
   submitMonthExport(){
-    
+
+  }
+
+  initializeForm(): void{
+
+  }
+
+  open(): void {
+    this.initializeForm();
+    this.modalService
+      .open(this.modalElement, {
+        ariaLabelledBy: 'modal-basic-title',
+        windowClass: 'custom-modal',
+      })
+      .result.then(
+        (result) => {
+
+        },
+        (reason) => {
+          //forse initialize form
+        }
+      );
   }
 }
--- frontend/xacria-presenze/src/app/modules/custom-calendar/components/excel-button/excel-button.component.ts
+++ frontend/xacria-presenze/src/app/modules/custom-calendar/components/excel-button/excel-button.component.ts
@@ -1,12 +1,20 @@
 import { Component, Input } from '@angular/core';
 import { faDownload } from '@fortawesome/free-solid-svg-icons';
+import { CalendarCsvModalComponent } from '../calendar-csv-modal/calendar-csv-modal.component';
 
 @Component({
   selector: 'app-excel-button',
   templateUrl: './excel-button.component.html',
-  styleUrls: ['./excel-button.component.scss']
+  styleUrls: ['./excel-button.component.scss'],
 })
 export class ExcelButtonComponent {
   @Input() buttonClass: string = '';
-  faDownload = faDownload
+  @Input() currentDate!: Date;
+  faDownload = faDownload;
+
+  constructor() {}
+
+  openExportModal(modal: CalendarCsvModalComponent) {
+    modal.open();
+  }
 }
